a873f16854f965c65d4a84f3ea337a95f0693ee1
Wayne <34191804+wkoh1024@users.noreply.github.com>
2025-06-10 15:54:30 -0400

    Added toDo input

--- src/modalHandler.js
+++ src/modalHandler.js
@@ -2,12 +2,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector("#new-proj-btn");
 const closeModalBtn = document.querySelector("#close-modal");
+const toDo = document.querySelector("#toDo");
 
-const openNCloseModal = function () {
+
+const toggleModal = function () {
   modal.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
 };
 
-openModalBtn.addEventListener("click", openNCloseModal);
-closeModalBtn.addEventListener("click", openNCloseModal);
+const toDoHandler = function () {
+    // TODO: create new div, take input value
+    // and assign to div, and then clear textbox
+    console.log("testingsdfds");
+};
+
+toDo.addEventListener("keydown", toDoHandler)
+openModalBtn.addEventListener("click", toggleModal);
+closeModalBtn.addEventListener("click", toggleModal);
 
