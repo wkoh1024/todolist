faf6e6d683fcc2c9bb9c4bc71db555806ec12cdb
Wayne <waynekoh1024@gmail.com>
2025-06-16 02:04:35 -0400

    Changed section to dialog for better handling

--- src/modalHandler.js
+++ src/modalHandler.js
@@ -1,19 +1,20 @@
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector("#new-proj-btn");
 const closeModalBtn = document.querySelector("#close-modal");
 const toDo = document.querySelector("#toDo");
 const toDoContainer = document.querySelector("#to-do");
+const dueDate = document.querySelector("#dueDate");
 
 
-const toggleModal = function () {
-  modal.classList.toggle("hidden");
-  overlay.classList.toggle("hidden");
+const openModal = function () {
+  modal.showModal();
+};
+
+const closeModal = function (e) {
+  modal.close();
 };
 
 const toDoHandler = function (e) {
-    // TODO: create new div, take input value
-    // and assign to div, and then clear textbox
     if ((e.key === 'Enter')) {
       let toDoInputBox = e.target;
       let toDoInputBoxText = toDoInputBox.value;
@@ -28,6 +29,6 @@ const toDoHandler = function (e) {
 };
 
 toDo.addEventListener("keydown", toDoHandler)
-openModalBtn.addEventListener("click", toggleModal);
-closeModalBtn.addEventListener("click", toggleModal);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
 
