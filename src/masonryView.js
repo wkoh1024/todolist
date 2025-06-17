f052bcebd24b502363d00831246d7a8b34bf7d2f
Wayne <waynekoh1024@gmail.com>
2025-06-17 11:38:16 -0400

    Commeneted out masonry and removed obsolete file

--- src/masonryView.js
+++ src/masonryView.js
@@ -1,8 +1,15 @@
-import Masonry from "masonry-layout";
+// import Masonry from "masonry-layout";
 
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
 
-let msny = new Masonry(container, {
-    itemSelector: '.project', 
-    columnWidth: 900,
-});
+// let msny = new Masonry(container, {
+//     itemSelector: '.project', 
+//     columnWidth: '.project',
+//     gutter: 40, 
+//     fitWidth: true, 
+//     transitionDuration: '0.2s'
+// });
+
+// window.addEventListener('load', () => {
+//     msny.layout();
+// });
