fc59a9512658eb5ec530d5e1d62e57b6b110a268
Wayne <34191804+wkoh1024@users.noreply.github.com>
2025-07-15 12:57:15 -0400

    Masonry layout changes upon individual project size changes

--- src/masonryView.js
+++ src/masonryView.js
@@ -1,17 +1,30 @@
 import Masonry from "masonry-layout";
 
 const container = document.querySelector("#container");
+const projects = document.querySelectorAll("div.project");
 
 let msny = new Masonry(container, {
     itemSelector: '.project', 
     columnWidth: '.project',
     gutter: 20, 
     fitWidth: true, 
-    transitionDuration: '0.1s'
+    transitionDuration: '0.2s',
+    horizontalOrder: true,
+    // fitWidth: true
+
 });
 
 msny.layout();
 
-container.addEventListener('change', () => {
-    msny.layout();
+const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+        msny.layout();
+        console.log("Resized:", entry.target);
+    }
 });
+
+function observeProjects() {
+    projects.forEach(project => resizeObserver.observe(project));
+}
+
+observeProjects();
