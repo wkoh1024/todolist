51d39bd636f27df4931a6a8e85a8b464c603998a
Wayne <34191804+wkoh1024@users.noreply.github.com>
2025-08-02 14:46:53 -0400

    Fixed inconsistent spacing in grid, fixed. FOUC on initial load

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import "./styles.css";
 import "./modalHandler.js"
 import { allProjects } from "./projectManager";
 
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.style.visibility = "visible";
+});
+
 console.log("connected!");
